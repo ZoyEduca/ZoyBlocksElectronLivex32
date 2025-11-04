6201e21c5d229541da8c8f4c9a6e83487e489f1e
Felipe Correia <felipecdbm@gmail.com>
2025-11-04 12:13:00 -0300

    feat: Cria dos blocos de se houver ou não houver obstaculos com sensor ir

--- src/assets/blocks/zoy/zoy_steam_blocks/cates/infravermelho.js
+++ src/assets/blocks/zoy/zoy_steam_blocks/cates/infravermelho.js
@@ -59,7 +59,7 @@
 
       {
         type: "ir_if_detect",
-        message0: "Se sensor IR no pino %1 detectar obstáculo",
+        message0: "Se sensor IR no pino %1 como %2 detectar obstáculo",
         args0: [
           {
             type: "field_dropdown",
@@ -73,6 +73,14 @@
               ["D10", "D10"],
               ["D12", "D12"],
               ["D13", "D13"],
+            ],
+          },
+          {
+            type: "field_dropdown",
+            name: "MODO",
+            options: [
+              ["INPUT", "INPUT"],
+              ["INPUT_PULLUP", "INPUT_PULLUP"],
             ],
           },
         ],
@@ -89,13 +97,54 @@
         tooltip: "Executa ações se o sensor detectar obstáculo",
         helpUrl: "",
       },
+
+      {
+        type: "ir_if_not_detect",
+        message0: "Se sensor IR no pino %1 como %2 NÃO detectar obstáculo",
+        args0: [
+          {
+            type: "field_dropdown",
+            name: "PIN",
+            options: [
+              ["A0/D14", "D14"],
+              ["A4/D18", "D18"],
+              ["A5/D19", "D19"],
+              ["D2", "D2"],
+              ["D9", "D9"],
+              ["D10", "D10"],
+              ["D12", "D12"],
+              ["D13", "D13"],
+            ],
+          },
+          {
+            type: "field_dropdown",
+            name: "MODO",
+            options: [
+              ["INPUT", "INPUT"],
+              ["INPUT_PULLUP", "INPUT_PULLUP"],
+            ],
+          },
+        ],
+        message1: "faça %1",
+        args1: [
+          {
+            type: "input_statement",
+            name: "DO"
+          }
+        ],
+        previousStatement: null,
+        nextStatement: null,
+        colour: COR_BLOCOS,
+        tooltip: "Executa ações se o sensor NÃO detectar obstáculo",
+        helpUrl: "",
+      },
     ]);
 
     // Geração de código Javascript
     Blockly.JavaScript.forBlock["ir_read"] = function (block) {
       const pin = block.getFieldValue("PIN");
-      // Adiciona "await" pois é uma função assíncrona
       const modo = block.getFieldValue("MODO");
+      // Adiciona "await" pois é uma função assíncrona
       const code = `await digital_read("DIGITAL_READ","${pin}, ${modo}")`;
       return [code, Blockly.JavaScript.ORDER_AWAIT];
     };
@@ -107,11 +156,23 @@
       return [code, Blockly.JavaScript.ORDER_AWAIT];
     };
 
-    Blockly.JavaScript.forBlock["ir_if_detect"] = (block) => {
+    Blockly.JavaScript.forBlock["ir_if_detect"] = function (block) {
       const pin = block.getFieldValue("PIN");
-      const statements = Blockly.JavaScript.forBlock.statementToCode(block, "DO");
-      return `if ir_read("${pin}"):\n${statements}`;
+      const modo = block.getFieldValue("MODO");
+      // Código dos blocos dentro do "faça"
+      const statements = Blockly.JavaScript.statementToCode(block, "DO");
+      const code = `if (await digital_read("DIGITAL_READ","${pin}, ${modo}")) {${statements}}`;
+      return code;
     };
+
+    Blockly.JavaScript.forBlock["ir_if_not_detect"] = function (block) {
+      const pin = block.getFieldValue("PIN");
+      const modo = block.getFieldValue("MODO");
+      const statements = Blockly.JavaScript.statementToCode(block, "DO");
+      const code = `if (!(await digital_read("DIGITAL_READ","${pin}, ${modo}"))) {${statements}}`;
+      return code;
+    };
+
   };
 
   // Categoria para toolbox
@@ -123,6 +184,7 @@
       { kind: "block", type: "ir_read" },
       { kind: "block", type: "ir_value" },
       { kind: "block", type: "ir_if_detect" },
+      { kind: "block", type: "ir_if_not_detect" },
     ],
   };
 
